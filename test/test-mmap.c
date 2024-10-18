3a21b7ba9d6d837f1981fd3d4866a6890a68fa69
Mohammad Khalaji <mohammad.khalaji@uwaterloo.ca>
2024-10-18 01:40:09 -0400

    working mmap example

--- test/test-mmap.c
+++ test/test-mmap.c
@@ -6,20 +6,18 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
-#define MB * 1024 * 1024
-#define FILE_SIZE (256 MB)  // 256MB
+#define MB *1024 * 1024
+#define FILE_SIZE (256 MB) // 256MB
 
-
-const char *text = "Hello, World!";
-
+const char* text = "Hello, World!";
 
 int main() {
     mi_option_enable(mi_option_verbose);
     mi_option_disable(mi_option_reserve_os_memory);
     mi_option_disable(mi_option_reserve_huge_os_pages);
-    mi_option_enable(mi_option_limit_os_alloc); 
+    mi_option_enable(mi_option_limit_os_alloc);
 
-    const char *filename = "/home/mohammad/repos/mimalloc/mmapped_file.txt";
+    const char* filename = "/home/mohammad/repos/mimalloc/mmapped_file.txt";
     int fd;
 
     printf("Creating a file with size %d\n", FILE_SIZE);
@@ -35,18 +33,18 @@ int main() {
         return 1;
     }
 
-
     printf("mmapping the file\n");
-    // make sure to align the mmapped memory to a multiple of the page size: MI_SEGMENT_SIZE
+    // make sure to align the mmapped memory to a multiple of the page size:
+    // MI_SEGMENT_SIZE
 
-    char *file_in_memory = mmap(NULL, FILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_32BIT, fd, 0);
+    char* file_in_memory = mmap(NULL, FILE_SIZE, PROT_READ | PROT_WRITE,
+                                MAP_SHARED | MAP_32BIT, fd, 0);
     if (file_in_memory == MAP_FAILED) {
         perror("Error mmapping the file");
         return 1;
     }
 
     close(fd);
-
 
     memset(file_in_memory, (char)0, FILE_SIZE);
     msync(file_in_memory, FILE_SIZE, MS_SYNC);
@@ -59,27 +57,26 @@ int main() {
 
     printf("mmap successful, passing it as an arena to mimalloc\n");
 
+    bool ret = mi_manage_os_memory((void*)file_in_memory, FILE_SIZE, true, true,
+                                   true, -1);
 
-    bool ret = mi_manage_os_memory((void *)file_in_memory, FILE_SIZE, true, true, true, -1);
-
-
-    void *p = mi_malloc(1 MB);
-    void *q = mi_malloc(1 MB);
-    void *r = mi_malloc(1 MB);
-    void *s = mi_malloc(1 MB);
+    void* p = mi_malloc(1 MB);
+    void* q = mi_malloc(1 MB);
+    void* r = mi_malloc(1 MB);
+    void* s = mi_malloc(1 MB);
 
     printf("base: %p\n", file_in_memory);
     printf("p   : %p\n", p);
     printf("q   : %p\n", q);
     printf("r   : %p\n", r);
     printf("s   : %p\n", s);
-
+ 
     // am I writing to the file or is mimalloc getting memory from elsewhere?
     memcpy(p, text, strlen(text));
 
     printf("freeing the memory\n");
 
-    int offset = (size_t) p - (size_t) file_in_memory;
+    int offset = (size_t)p - (size_t)file_in_memory;
     for (int i = offset; i < offset + strlen(text); i++) {
         printf("i: %d, buf[i]: %c\n", i, file_in_memory[i]);
     }
@@ -105,7 +102,7 @@ int main() {
 
     // print the bytes you wrote to the file
 
-    char *buf = (char *)malloc(FILE_SIZE);
+    char* buf = (char*)malloc(FILE_SIZE);
     if (read(fd, buf, FILE_SIZE) == -1) {
         perror("Error reading from file");
         return 1;
@@ -114,12 +111,10 @@ int main() {
     // print bytes from p to p+strlen(text)
     printf("Printing the bytes from the file\n");
 
-
     for (int i = offset; i < offset + strlen(text); i++) {
         printf("i: %d, buf[i]: %c\n", i, buf[i]);
     }
     printf("\n");
-
 
     return 0;
 }
--- test/test-new-mmap.c
+++ test/test-new-mmap.c
@@ -1,52 +1,72 @@
 #include <assert.h>
 #include <fcntl.h>
-#include <mimalloc.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <mimalloc.h>
+#include "mimalloc/internal.h"
+#include "mimalloc/atomic.h"
+#include "mimalloc/prim.h"   // _mi_prim_thread_id()
+
+#define THREAD_COUNT 24
+
+size_t total_pmem_size = 1 * 1024 * 1024 * 1024ULL;
+void* addr = NULL;
+
+void* thread_func(const int tid) {
+    mi_heap_t* defaultHeap = mi_heap_get_default();
+    int garbage = 0;
+    for (int i = 0; i < (THREAD_COUNT - tid) * 1000000; i++) {
+        garbage += i / 2;
+    }
+    printf("Thread=%d, defaultHeap=%p\n", tid, defaultHeap);
+    for (int i = 0; i < 100; i++) { 
+        void *p = mi_malloc(2240);
+        printf("Thread=%d, allocated=%p\n", tid, p);
+    }
+}
 
 int main() {
     mi_option_enable(mi_option_verbose);
     mi_option_disable(mi_option_reserve_os_memory);
     mi_option_disable(mi_option_reserve_huge_os_pages);
     mi_option_enable(mi_option_limit_os_alloc);
+
     mi_option_enable(mi_option_disallow_os_alloc);
     mi_option_set(mi_option_arena_reserve, 0);
 
-    size_t total_pmem_size = 1 * 1024 * 1024 * 1024ULL;
+
+    // OPEN AND TRUNCATE THE FILE
     int fd = open("/mnt/pmem1_mount/mkhalaji/mimalloc_playground/file.dat",
                   O_RDWR | O_CREAT, 0666);
     ftruncate(fd, total_pmem_size);
 
-    void* addr = mmap(0, total_pmem_size, PROT_READ | PROT_WRITE,
-                      MAP_SYNC | MAP_SHARED_VALIDATE, fd, 0);
+    // MMAP THE FILE
+    addr = mmap(0, total_pmem_size, PROT_READ | PROT_WRITE,
+                MAP_SYNC | MAP_SHARED_VALIDATE, fd, 0);
 
-    bool ret =
-        mi_manage_os_memory((void*)addr, total_pmem_size, true, true, false, -1);
+    printf("Start of the file: %p\n", addr); 
+
+
+    bool ret = mi_manage_os_memory((void*)addr, total_pmem_size, true, false,
+                                   false, -1);
 
     printf("mi_manage_os_memory returned %d\n", ret);
 
-    printf("Start of the file: %p\n", addr);
 
-    // set up random generator: 
-    srand(1234); 
+    void *p = mi_malloc(2048);
+
+    printf("Allocated: %p\n", p);
 
 
-
-    for (size_t i = 0; i < 1024 * 1024; i++) {
-        void* ptr = mi_malloc(1 * 1024ULL);
-
-        assert(ptr != NULL);
-
-        assert(ptr >= addr && ptr < (void*)((char*)addr + total_pmem_size));
-
-        if (rand() % 2 == 0) {
-            mi_free(ptr);
-        }
-
-        if (i % 1000 == 0) {
-            printf("Allocated %lu\n", i);
-        }
+    pthread_t threads[THREAD_COUNT];
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        pthread_create(&threads[i], NULL, thread_func, i);
+    }
+    // wait for all threads to finish
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        pthread_join(threads[i], NULL);
     }
 
     printf("Finished\n");
